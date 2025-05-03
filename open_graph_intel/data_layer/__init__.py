681a6b5b34679ed72f5e7da7ad5ffd632abf023e
Edward Cromwell <edwardlc@umich.edu>
2025-05-03 10:44:59 -0400

    chore: init.py file for source package

--- open_graph_intel/data_layer/__init__.py
+++ open_graph_intel/data_layer/__init__.py
@@ -1,1 +1,12 @@
+# open_graph_intel/data_layer/db.py
 
+"""
+This module provides the database connection and session management for open_graph_intel.
+"""
+
+from .db import (
+    get_db,
+    engine,
+    SessionLocal,
+    Base
+)
