c3b8859ebee92446ed94dc5beb8fc5a263f14611
Edward Cromwell <edwardlc@umich.edu>
2025-05-10 07:51:02 -0400

    refactor (data_layer/database): prevent code from running on import

--- open_graph_intel/data_layer/database.py
+++ open_graph_intel/data_layer/database.py
@@ -2,7 +2,7 @@
 
 # Import dependencies
 import logging
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from typing import Final
 
@@ -18,32 +18,90 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Retrieve the environment variables with detailed error information
-try:
-    DB_NAME: Final[str] = get_env_variable("POSTGRES_DB")
-    USER: Final[str] = get_env_variable("POSTGRES_USER")
-    PASSWORD: Final[str] = get_env_variable("POSTGRES_PASSWORD")
-    HOST: Final[str] = get_env_variable("POSTGRES_HOST")
-    PORT: Final[str] = get_env_variable("POSTGRES_PORT")
-except ValueError as e:
-    logger.error(f"Error retrieving environment variables: {e}")
-    raise
+# Cache for the engine and session
+_session_factory = None
+_engine = None
 
-# Construct the PostgreSQL URL
-DATABASE_URL: Final[str] = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+# Lazy initialization for the database URL
+def construct_postgres_url() -> str:
+    """
+    Constructs the PostgreSQL URL from environment variables.
+    Returns:
+        str: The PostgreSQL URL.
+    """
+    try:
+        DB_NAME = get_env_variable("POSTGRES_DB")
+        USER = get_env_variable("POSTGRES_USER")
+        PASSWORD = get_env_variable("POSTGRES_PASSWORD")
+        HOST = get_env_variable("POSTGRES_HOST")
+        PORT = get_env_variable("POSTGRES_PORT")
+        return f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+    except ValueError as e:
+        logger.error(f"Error retrieving environment variables: {e}")
+        raise
 
-# Create the database engine
-try:
-    engine = create_engine(DATABASE_URL)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    Base = declarative_base()
-    logger.info("Database engine created successfully.")
-except Exception as e:
-    logger.error(f"Error creating database engine: {e}")
-    raise
+# Lazy intialization of the database engine
+def construct_engine(database_url: str = None, retries: int = 5) -> Engine:
+    """
+    Creates a database engine with retry logic.
+    Args:
+        database_url (str): The database URL.
+        retries (int): Number of retries for creating the engine.
+    Returns:
+        engine: The SQLAlchemy engine.
+    """
+    global _engine
+    if _engine is None:
+        if database_url is None:
+            database_url = construct_postgres_url()
+        for i in range(retries):
+            try:
+                _engine = create_engine(database_url)
+                logger.info(f"Database engine created successfully: {database_url('@')[-1]}")
+                return _engine
+            except Exception as e:
+                logger.error(f"Error creating database engine: {e}")
+                if i < retries - 1:
+                    logger.info(f"Retrying to create database engine... ({i + 1}/{retries})")
+        logger.error(f"Failed to create database engine after {retries} retries.")
+        raise
+    return _engine
+
+def construct_session(engine: Engine = None) -> sessionmaker:
+    """
+    Creates a session for the database.
+    Args:
+        engine: The SQLAlchemy engine.
+    Returns:
+        SessionLocal: The session factory.
+    """
+    global _session_factory
+    if _session_factory is None:
+        logger.info("Creating a new session factory.")
+        if engine is None:
+            engine = construct_engine()
+        _session_factory = sessionmaker(autocommit=False,
+                                        autoflush=False,
+                                        bind=engine)
+    return _session_factory
 
 
+# Lazy initialization for the declarative base
+def construct_base() -> declarative_base:
+    """
+    Creates a declarative base for the database models.
+    Returns:
+        Base: The declarative base.
+    """
+    return declarative_base()
+
+
+# Dependency injection for the database session
 def get_db():
+    """
+    Dependency that provides a database session.
+    """
+    SessionLocal = construct_session()
     db = SessionLocal()
     try:
         yield db
