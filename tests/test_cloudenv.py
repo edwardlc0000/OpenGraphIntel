2e5421f34cd1067dfdd1e480202b758b1e6ccbf5
Edward Cromwell <edwardlc@umich.edu>
2025-05-25 10:27:14 -0400

    test (common/cloud_env): improved test coverage for the cloud environment detection, added function stems for remaining tests

--- tests/test_cloudenv.py
+++ tests/test_cloudenv.py
@@ -3,25 +3,129 @@
 import pytest
 import requests
 from unittest.mock import patch, MagicMock
-import backend.common.cloud_env
-
-def test_detect_server_env():
-    with patch('requests.get') as mock_get:
-        mock_get.return_value.status_code = 200
-        assert backend.common.cloud_env.detect_server_env() is True
+import backend.common.cloud_env as cloud_env
 
 def test_detect_server_env_failure():
     with patch('requests.get') as mock_get:
         mock_get.side_effect = requests.RequestException
-        assert backend.common.cloud_env.detect_server_env() is False
+        assert cloud_env.detect_server_env() is False
 
 def test_detect_aws_metadata():
     with patch('requests.get') as mock_get:
         mock_get.return_value.status_code = 200
-        assert backend.common.cloud_env.detect_aws_metadata() is True
+        assert cloud_env.detect_aws_metadata() is True
+
+def test_detect_server_env():
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 200
+        assert cloud_env.detect_server_env() is True
 
 def test_detect_aws_metadata_failure():
     with patch('requests.get') as mock_get:
         mock_get.side_effect = requests.RequestException
-        assert backend.common.cloud_env.detect_aws_metadata() is False
+        assert cloud_env.detect_aws_metadata() is False
 
+def test_detect_azure_metadata():
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 200
+        assert cloud_env.detect_azure_metadata() is True
+
+def test_detect_azure_metadata_failure():
+    with patch('requests.get') as mock_get:
+        mock_get.side_effect = requests.RequestException
+        assert cloud_env.detect_azure_metadata() is False
+
+def test_dectect_gcp_metadata():
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 200
+        assert cloud_env.detect_gcp_metadata() is True
+
+def test_detect_gcp_metadata_failure():
+    with patch('requests.get') as mock_get:
+        mock_get.side_effect = requests.RequestException
+        assert cloud_env.detect_gcp_metadata() is False
+
+def test_detect_server_provider_aws():
+    with patch('backend.common.cloud_env.detect_aws_metadata', return_value=True):
+        assert cloud_env.detect_server_provider() == "aws"
+
+def test_detect_server_provider_azure():
+    with patch('backend.common.cloud_env.detect_azure_metadata', return_value=True):
+        assert cloud_env.detect_server_provider() == "azure"
+
+def test_detect_server_provider_gcp():
+    with patch('backend.common.cloud_env.detect_gcp_metadata', return_value=True):
+        assert cloud_env.detect_server_provider() == "gcp"
+
+def test_detect_server_provider_unsupported():
+    with patch('backend.common.cloud_env.detect_aws_metadata', return_value=False), \
+         patch('backend.common.cloud_env.detect_azure_metadata', return_value=False), \
+         patch('backend.common.cloud_env.detect_gcp_metadata', return_value=False):
+        with pytest.raises(RuntimeError, match="Unsupported cloud provider detected."):
+            cloud_env.detect_server_provider()
+
+def test_detect_k8s_env():
+    with patch('os.path.exists', return_value=True):
+        assert cloud_env.detect_k8s_env() is True
+
+def test_detect_k8s_env_failure():
+    with patch('os.path.exists', return_value=False):
+        assert cloud_env.detect_k8s_env() is False
+
+def test_detect_k8s_provider_aws():
+    pass
+
+def test_detect_k8s_provider_azure():
+    pass
+
+def test_detect_k8s_provider_gcp():
+    pass
+
+def test_detect_k8s_provider_unsupported():
+    pass
+
+def test_detect_severless_env_aws(monkeypatch):
+    monkeypatch.setenv("AWS_EXECUTION_ENV", "test_function")
+    assert "aws" in cloud_env.detect_serverless_env()
+
+def test_detect_severless_env_azure(monkeypatch):
+    monkeypatch.setenv("AZURE_FUNCTIONS_ENVIRONMENT", "test_environment")
+    assert "azure" in cloud_env.detect_serverless_env()
+
+def test_detect_severless_env_gcp(monkeypatch):
+    monkeypatch.setenv("FUNCTION_NAME", "test_function")
+    assert "gcp" in cloud_env.detect_serverless_env()
+
+def test_detect_severless_env_unsupported():
+    with pytest.raises(RuntimeError, match="Unsupported cloud provider detected."):
+        cloud_env.detect_serverless_env()
+
+def test_detect_env_aws_server():
+    pass
+
+def test_detect_env_aws_k8s():
+    pass
+
+def test_detect_env_aws_lambda():
+    pass
+
+def test_detect_env_azure():
+    pass
+
+def test_detect_env_azure_k8s():
+    pass
+
+def test_detect_env_azure_functions():
+    pass
+
+def test_detect_env_gcp():
+    pass
+
+def test_detect_env_gcp_k8s():
+    pass
+
+def test_detect_env_gcp_functions():
+    pass
+
+def test_detect_env_unsupported():
+    pass
